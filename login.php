85abac3f2270e82cb4a9cb3b40289492b9cf18f5
tmd22121999 <37409790+tmd22121999@users.noreply.github.com>
2020-05-04 04:15:55 +0700

    Update login.php

--- login.php
+++ login.php
@@ -36,6 +36,8 @@
 		//print_r($uid);
 		if (!($arrResp['userId']==-1)){
 			$_SESSION["UID"]=$arrResp;
+			$message = $_SESSION["UID"];
+			echo "<script type='text/javascript'>alert('$message');</script>";
 			curl_close($curl);
 			header("Location: index.php");
 			echo $uid;

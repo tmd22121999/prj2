0c3d989db6b5b258a6305a5603322487de265305
tmd22121999 <37409790+tmd22121999@users.noreply.github.com>
2020-05-04 04:07:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
     header("Location: login.php");
     exit;
 	}
-	echo $_SESSION["UID"];
+	$uid=$_SESSION["UID"]
+	echo $uid;
 	
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">

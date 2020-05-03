30a9f9b26f920a840fd583d77fea2734169df04c
tmd22121999 <37409790+tmd22121999@users.noreply.github.com>
2020-05-04 04:27:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     exit;
 	}
 	$uid=$_SESSION["UID"];
-	echo $uid;
+	print($uid);
 	
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">

1ee131e09c9cde99db20f2afe12ac0c7200ade6c
tmd22121999 <37409790+tmd22121999@users.noreply.github.com>
2020-05-09 09:16:29 +0700

    Add files via upload

---
+++ tuvan.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Yêu cầu tư vấn</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<?php require "header.html"; ?>
+	<div id="main">
+		<form action="tuvan.php" method="post" name="yctuvan">'
+        <div> Mời nhập yêu cầu tư vấn của bạn </div>
+        <textarea name="tuvan" cols="50" rows="20"></textarea>
+      </form>
+    </div>
+</body>
+</html>

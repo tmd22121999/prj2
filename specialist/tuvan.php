078fa996887b72e4421e35efc8271148c64b85dc
tmd22121999 <37409790+tmd22121999@users.noreply.github.com>
2020-05-20 07:07:09 +0700

    Add files via upload

--- specialist/tuvan.php
+++ specialist/tuvan.php
@@ -9,11 +9,38 @@
 <body>
 	<?php require "header.html"; ?>
 	<div id="main">
-		<form action="tuvan.php" method="post" name="yctuvan">'
-        <div> Mời nhập yêu cầu tư vấn của bạn </div>
-        <textarea name="tuvan" cols="50" rows="20"></textarea>
+     	<p></p><br />
+        <div class=" content">  
+       		<h3>Chi tiết yêu cầu tư vấn</h3><br />
+             <div class="row">
+                <div class="col-md-4"><img  src="https://i.stack.imgur.com/FaRRO.jpg?s=32&g=1" alt="Smiley face" height="250" width="250"></div>
+                <div class="col-md-8"><h4>Mô tả chi tiết</h4>
+                    <div id="chitiet" style="width:90%">
+                    	hfaflsjfsnfsakejlnd.sknflewfnk.hfaflsjfsnfsakejlnd.sknflewfnk.hfaflsjfsnfsakejlnd.sknflewfnk.hfaflsjfsnfsakejlnd.sk
+                    </div>
+                    <a href="#reply" style=" position:absolute; bottom:-50px; right:0;" class=" btn btn-primary" >Trả lời tư vấn</a>
+                 </div>
+            </div>
+        </div>
+
+		<form action="tuvan.php" method="post" class="content" name="yctuvan">
+        <h3 id="reply" class=" show fade"> Nhập trả lời tư vấn </h3><br />
+          <div class="row">
+  			<div class="col-md"> <div class="form-group">
+          <label for="chuandoan"><h5>Chuẩn đoán:</h5></label>
+          <textarea name="chuandoan" class="form-control" rows="7" id="chuandoan"></textarea>
+        </div></div>
+  			<div class="col-md"><div class="form-group">
+          <label for="kedon"><h5>Kê đơn:</h5></label>
+          <textarea name="kedon" class="form-control" rows="7" id="kedon"></textarea>
+        </div> </div>
+		</div>
+        <div class="form-group">
+          <label for="ketluan"><h5>kết luận:</h5></label>
+          <textarea name="keluan" class="form-control" rows="4" id="ketluan"></textarea>
+        </div>
+        <input value="Gửi cho bệnh nhân"  class=" btn btn-success " name="send" type="submit">
       </form>
-      <form action="tuvan.php" method="get"><input name="" type="submit"></form>
     </div>
 </body>
 </html>

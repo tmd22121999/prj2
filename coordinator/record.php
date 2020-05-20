afa0e204cbe1bcbc478d37c5cb0c118b89f84aba
tmd22121999 <37409790+tmd22121999@users.noreply.github.com>
2020-05-20 07:07:43 +0700

    Add files via upload

--- coordinator/record.php
+++ coordinator/record.php
@@ -8,28 +8,59 @@
 <body>
 <?php require "header.html"; ?>
 <div id="main" class="container">
-
-      <h3> Danh sách tư vấn của bệnh nhân mới chưa được gán</h3>
-	<table class="table table-hover">
-    <thead>
-      <tr>
-        <th>Họ tên</th>
-        <th>Mô tả</th>
-        <th>gì đó</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Bệnh nhân 01</td>
-        <td>Đau bụng</td>
-        <td>
-        	<a href="#" class=" btn btn-success" >Gán bệnh nhân</a>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-    </div>
     
+   	 <ul class="nav nav-tabs" role="tablist">
+        <li class="nav-item">
+          <a class="nav-link active" data-toggle="tab" href="#newpa">Tư vấn của bệnh nhân mới</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="tab" href="#newinj">Tất cả yêu cầu tư vấn</a>
+        </li>
+      </ul>
+    <div class="tab-content">
+        <div id="newpa" class="container tab-pane active"><br>
+          <h3> Yêu cầu tư vấn của bệnh nhân mới</h3>
+          <p>Danh sách hững yêu cầu tư vấn của bệnh nhân mới chưa được gán cho bác sĩ đã khoa: </p>
+        <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>Họ tên</th>
+            <th>Mô tả</th>
+            <th>gì đó</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Bệnh nhân 01</td>
+            <td>Đau bụng</td>
+            <td>
+                <a href="#" class=" btn btn-success" >Gán bệnh nhân</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+        </div>
+        <div id="newinj" class="container tab-pane fade"><br>
+          <h3> Danh sách yêu cầu tư vấn </h3>
+          <p>Danh sách tất cả yêu cầu tư vấn của bệnh nhân đã được gửi đến bệnh viện:</p>
+            <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>Họ tên</th>
+                <th>Mô tả</th>
+                <th>gì đó</th>
+              </tr>
+            </thead>
+            	<tbody>
+              		<tr>
+                	<td>Bệnh nhân 01</td>
+               		 <td>Đau bụng</td>
+               		 <td><a href="tuvan.php" class="nav-link" >Xem chi tiết</a></td>
+					</tr>
+            	</tbody>
+          	</table>
+        </div>
+    </div>
 </div>
 </body>
 </html>
